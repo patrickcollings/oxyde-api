fd8925e753c81602c8c1bc544cd19c4140786093
Patrick <pc8229@exeter.ac.uk>
2019-09-24 14:16:51 +0100

    Set up for push to heroku

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 ﻿require('rootpath')();
+require('dotenv').config()
+
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -23,6 +25,8 @@ app.use(errorHandler);
 
 // start server
 const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
-const server = app.listen(port, function () {
+app.listen(port, function () {
     console.log('Server listening on port ' + port);
 });
+
+module.exports = app;
