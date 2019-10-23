f0066e41ac1c127c633568ed99dc04e12fafa311
Patrick <pc8229@exeter.ac.uk>
2019-10-23 16:48:42 +0100

    Fixed employee not being deleted completely

--- employees/employee.service.js
+++ employees/employee.service.js
@@ -50,8 +50,8 @@ async function update(id, param, managerId) {
         throw 'Employee email already in use';
     }
 
-    console.log(employee);
-    console.log(managerId);
+    // console.log(employee);
+    // console.log(managerId);
     // Check manager has access to employee
     if (employee.manager != managerId) {
         throw 'Manager does not have access to this employee';
@@ -72,6 +72,11 @@ async function _delete(id, managerId) {
     if (employee.manager != managerId) {
         throw 'Manager does not have access to this employee';
     }
+
+    // Remove employee from manager
+    const manager = await Manager.findById(managerId);
+    await manager.employees.pull({ _id: id });
+    await manager.save();
 
     // Remove employee
     await Employee.findByIdAndRemove(id);
